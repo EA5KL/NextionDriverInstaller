a553c9ba9d1f60dbef37714874ffb744a74b1394
ElGamal <706513+ElGamal@users.noreply.github.com>
2019-08-19 16:51:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,11 +23,11 @@ cd /tmp
 sudo apt-get update
 sudo apt-get install libcurl4-openssl-dev
 cd /lib
-sudo rsync -rl --safe-links pi-star@192.168.0.11:/lib/arm-linux-gnueabihf .
+sudo rsync -rl --safe-links localhost:/lib/arm-linux-gnueabihf .
 cd /usr/lib
-sudo rsync -rl pi-star@localhost:/usr/lib/arm-linux-gnueabihf .
+sudo rsync -rl localhost:/usr/lib/arm-linux-gnueabihf .
 cd /usr/local/include
-sudo rsync -rl --safe-links pi-star@localhost:/usr/include/arm-linux-gnueabihf .
+sudo rsync -rl --safe-links localhost:/usr/include/arm-linux-gnueabihf .
 
 echo "+ Getting NextionDriver ..."
 cd /tmp
